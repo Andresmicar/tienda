c214e85897fee0593392ff80d66a986ebcb3852f
Admin <admin@iMac-de-Admin.local>
2020-05-28 16:19:23 +0200

    Primera actualizacion

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -1,40 +1,54 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { TabsPage } from './tabs.page';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { TabsPage } from "./tabs.page";
 
 const routes: Routes = [
   {
-    path: 'tabs',
+    path: "tabs",
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: "Dashboard",
+        loadChildren: () =>
+          import("../pages/dashboard/dashboard.module").then(
+            (m) => m.DashboardModule
+          ),
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        path: "Settings",
+        loadChildren: () =>
+          import("../pages/settings/settings.module").then(
+            (m) => m.SettingsModule
+          ),
       },
       {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        path: "Budget",
+        loadChildren: () =>
+          import("../pages/budget/budget.module").then((m) => m.BudgetModule),
       },
       {
-        path: '',
-        redirectTo: '/tabs/tab1',
-        pathMatch: 'full'
-      }
-    ]
+        path: "Activity",
+        loadChildren: () =>
+          import("../pages/activity/activity.module").then(
+            (m) => m.ActivityModule
+          ),
+      },
+      {
+        path: "",
+        redirectTo: "/tabs/Dashboard",
+        pathMatch: "full",
+      },
+    ],
   },
   {
-    path: '',
-    redirectTo: '/tabs/tab1',
-    pathMatch: 'full'
-  }
+    path: "",
+    redirectTo: "/tabs/Dashboard",
+    pathMatch: "full",
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class TabsPageRoutingModule {}
--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -1,19 +1,27 @@
-import { IonicModule } from '@ionic/angular';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { IonicModule } from "@ionic/angular";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
-import { TabsPageRoutingModule } from './tabs-routing.module';
+import { TabsPageRoutingModule } from "./tabs-routing.module";
 
-import { TabsPage } from './tabs.page';
+import { TabsPage } from "./tabs.page";
+import { DashboardModule } from "../pages/dashboard/dashboard.module";
+import { SettingsModule } from "../pages/settings/settings.module";
+import { ActivityModule } from "../pages/activity/activity.module";
+import { BudgetModule } from "../pages/budget/budget.module";
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    TabsPageRoutingModule
+    TabsPageRoutingModule,
+    DashboardModule,
+    SettingsModule,
+    ActivityModule,
+    BudgetModule,
   ],
-  declarations: [TabsPage]
+  declarations: [TabsPage],
 })
 export class TabsPageModule {}
